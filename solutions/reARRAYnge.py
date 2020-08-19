8f56b8cb19958bfdf757479120d3474ead0d19ce
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-08-19 23:53:50 +0530

    rearrange array psuedo code

---
+++ solutions/reARRAYnge.py
@@ -0,0 +1,11 @@
+import math
+
+# 1. Get input array
+# 2. sort array in ascending order
+# 3. calculate length of the array
+# 4. calculate no. of even and odd positions
+# 5. get highest n  number of digits where n = no. of even positions
+# 6. initialise output array
+# 7. place result(6) at even positions in ascending order in output array
+# 8. get lowest m number of digits where m = no. of odd positions
+# 9. place result(7) at odd positions in descending order in output array
